bc8f7ff17287d657ac55347173fab3721cdef6bc
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-04-12 14:51:30 -0400

    add spike test

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+import matplotlib.pyplot as plt
 
 @cocotb.test()
 async def test_project(dut):
@@ -44,3 +45,19 @@ async def test_reset(dut):
   await ClockCycles(dut.clk, 1)
   assert dut.uo_out.value
   dut._log.info("Done")
+
+
+@cocotb.test()
+async def test_spike(dut):
+  clock = Clock(dut.clk, 10, units="us")
+  cocotb.start_soon(clock.start())
+
+  dut._log.info("Reset")
+  dut.ena.value = 1
+  dut.rst_n.value = 0
+  await ClockCycles(dut.clk, 1)
+  dut.rst_n.value = 1
+  dut.uio_in.value = 255
+  dut.ui_in.value = 255
+  await ClockCycles(dut.clk, 1)
+  assert dut.uo_out.value > 30 
