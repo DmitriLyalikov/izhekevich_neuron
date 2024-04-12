da95fff6991a0288c16dbab8163609b51da97405
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-04-12 16:33:52 -0400

    Update exai_izhekevich_neuron.v

--- src/exai_izhekevich_neuron.v
+++ src/exai_izhekevich_neuron.v
@@ -45,7 +45,7 @@ module tt_um_exai_izhekevich_neuron (
   assign a = uio_in[3:0];
   assign b = uio_in[7:4];
   assign c = 18'shF_6666;   // 5.4
-  assign d = 18'shE_4CCD;   // 0.2
+  assign d = 18'sh0_4CCD;   // 0.2
   assign p = 18'sh10_6666;   // 30
   assign c14 = 18'sh1_6666; // 1.4
   // Set the input of ui_in[7:0] to last 8 bits of I, 
