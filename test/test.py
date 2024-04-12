e4559f4bfd3dd905743858a897ec91f28dfc0669
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-04-12 15:55:45 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -57,7 +57,7 @@ async def test_spike(dut):
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 1)
   dut.rst_n.value = 1
-  dut.uio_in.value = 200
+  dut.uio_in.value = 20
   dut.ui_in.value = 255
   dut._log.info(dut.uo_out.value.integer)
   await ClockCycles(dut.clk, 1)
